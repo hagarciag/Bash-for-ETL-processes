7578090eebc007d381b9cda6981998375a8eaa4d
Valentina Vásquez Hernandez <31630385+vavasquezhe@users.noreply.github.com>
2018-03-02 08:47:39 -0500

    Add files via upload

--- quiz-sql/data.sql
+++ quiz-sql/data.sql
@@ -1,12 +1,12 @@
 DROP DATABASE IF EXISTS quiz;
 CREATE DATABASE quiz;
 USE quiz;
-CREATE TABLE IF NOT EXISTS franquicia(ccard VARCHAR(16),ccn VARCHAR(19),key1 INT,pin INT,quota INT,validthru DATE);
-CREATE TABLE IF NOT EXISTS emisor(Bank VARCHAR(29),ccn VARCHAR(19),ssn VARCHAR(11)); 
+CREATE TABLE IF NOT EXISTS franquicia(ccntype VARCHAR(16),ccn VARCHAR(19),validthru DATE, key1 INT,pin INT,quota INT);
+CREATE TABLE IF NOT EXISTS banco(bank VARCHAR(29),ccn VARCHAR(19),ssn VARCHAR(11)); 
 CREATE TABLE IF NOT EXISTS persona(ssn VARCHAR(15),phone VARCHAR(16),city VARCHAR(25),maritalstatus VARCHAR(10),fullname VARCHAR(20),birthdate DATE);
-LOAD DATA LOCAL INFILE 'insertar aquí la ruta' INTO TABLE emisor FIELDS TERMINATED BY ',' IGNORE 1 LINES (bank,ccn,ssn);
-LOAD DATA LOCAL INFILE 'insertar aquí la ruta' INTO TABLE franquicia FIELDS TERMINATED BY ',' IGNORE 1 LINES (ccard,ccn,key1,pin,quota,validthru);
-LOAD DATA LOCAL INFILE 'insertar aquí la ruta' INTO TABLE persona FIELDS TERMINATED BY ',' IGNORE 1 LINES (ssn,phone,city,maritalstatus,fullname,birthdate);
+LOAD DATA LOCAL INFILE 'C:/Users/Unalmed/Desktop/pruebasql/banco.csv' INTO TABLE banco FIELDS TERMINATED BY ',' IGNORE 1 LINES (bank,ccn,ssn);
+LOAD DATA LOCAL INFILE 'C:/Users/Unalmed/Desktop/pruebasql/franquicia.csv' INTO TABLE franquicia FIELDS TERMINATED BY ',' IGNORE 1 LINES (ccntype,ccn,validthru,key1,pin,quota);
+LOAD DATA LOCAL INFILE 'C:/Users/Unalmed/Desktop/pruebasql/persona.csv' INTO TABLE persona FIELDS TERMINATED BY ',' IGNORE 1 LINES (ssn,phone,city,maritalstatus,fullname,birthdate);
 
 
 
